c9b5faf1a1b79256eec26577e78bf32446bbee30
adivikramp <adileads1@gmail.com>
2024-12-15 20:40:53 +0530

    Added Google Authentication using NextAuth(Auth.js)

--- Next/wild-oasis-nextjs/app/account/page.jsx
+++ Next/wild-oasis-nextjs/app/account/page.jsx
@@ -1,13 +1,18 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Account",
 };
 
-const Account = () => {
-    return (
-        <div>
-          <h1>Account page</h1>
-        </div>
-      );
+const Account = async () => {
+  const session = await auth();
+  const firstName = session.user.name.split(" ").at(0);
+
+  return (
+    <h2 className="font-semibold text-2xl text-accent-700 mb-7">
+      Welcome, {firstName}
+    </h2>
+  );
 };
 
 export default Account;
